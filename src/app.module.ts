a027cd8d18d8c6e9cb5bfd97a3f78cffe893d494
abdifitahabdulkadir <abdifitahabdulkadir22@gmail.com>
2026-05-08 18:24:17 +0300

    add middlware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,34 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import middleware1, { AppMiddleware } from 'middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CustomerModule } from './customer/customer.module';
-import { StudentModule } from './student/student.module';
 import { GlobalModule } from './global/global.module';
+import { StudentModule } from './student/student.module';
 
 @Module({
   imports: [CatsModule, CustomerModule, StudentModule, GlobalModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+/**
+ * this is the module class that implements NestModule interface.
+ * and it is used to configure the middleware for the app.
+ * and we apply all routes. we can specify specific routes too and
+ * its method incase we need to do so.
+ */
+
+// class based is bettter when you have dependencies on other modules.
+export class AppModule implements NestModule {
+  /**
+   * Attaches either routes or controllers
+   * to the current middleware. If you pass a controller class,
+   * Nest will attach the current middleware to every path defined within it.
+   * we can pass a string to forRoutes to apply the middleware to all routes.
+   **/
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AppMiddleware, middleware1).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  // app.use(middleware1);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
